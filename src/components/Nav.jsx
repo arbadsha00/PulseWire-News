92c1cc3010f67459669138234b89efcb81248e47
arbadsha00 <arbadsha00@gmail.com>
2024-11-14 09:20:19 +0600

    nav done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { RiUser4Line } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div>
-           
+    const links = <>
+        <li>
+            <NavLink className={({ isActive }) =>
+          isActive
+            ? "bg-blue-500 text-white font-semibold" // Active color and style
+            : "text-gray-700 hover:text-blue-500" // Default color
+        } to="/">Home</NavLink>
+        </li>
+        <li>
+            <NavLink className={({ isActive }) =>
+          isActive
+            ? "bg-blue-500 text-white font-semibold" // Active color and style
+            : "text-gray-700 hover:text-blue-500" // Default color
+        } to="/career">Career</NavLink>
+        </li>
+        <li>
+            <NavLink className={({ isActive }) =>
+          isActive
+            ? "bg-blue-500 text-white font-semibold" // Active color and style
+            : "text-gray-700 hover:text-blue-500" // Default color
+        } to="/about">About</NavLink>
+        </li>
+       
+    </>
+  return (
+    <div className="navbar  p-0 bg-base-100 container mx-auto w-11/12 my-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
         </div>
-    );
+        <div></div>
+      </div>
+      <div className="navbar-center hidden md:flex">
+        <ul className="menu menu-horizontal px-1">
+        {links}
+        </ul>
+      </div>
+          <div className="navbar-end">
+         
+        <a className="btn font-semibold text-xl bg-blue-500 text-white rounded-none"> <RiUser4Line className="text-3xl " /> Login</a>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
