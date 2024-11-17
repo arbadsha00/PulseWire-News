182118942ab241221db97f02aa805459ff9e9ca7
arbadsha00 <arbadsha00@gmail.com>
2024-11-17 12:15:53 +0600

    error page done

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
-
+import { Link, useRouteError } from 'react-router-dom';
+import notFound from "../assets/not found.png"
 const Error = () => {
-    const error = useRouteError();
+  const error = useRouteError();
     return (
-        <div>
-            <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+        <div className='flex flex-col items-center relative'>
+        <img className='' src={notFound} alt="" />
+        <Link to='/' className='btn px-10 absolute bottom-[25%] md:bottom-[30%] shadow-xl bg-gradient-to-br from-cyan-400 via-indigo-500 to-pink-500 text-white text-xl border-none'>Home</Link>
         </div>
     );
 };
